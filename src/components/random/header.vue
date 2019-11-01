69e1f6b6aac2195cd5301c0b55d4d92ca74c3ef5
zqartwork <zqartworkofficial@gmail.com>
2019-11-01 23:41:13 +0800

    ADD Random seats

---
+++ src/components/random/header.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="title">Random Seats for Hualien</div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 24px;
+        font-weight: 700;
+    }
+</style>
